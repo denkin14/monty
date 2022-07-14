9cb47af13d6da577d742b6f8f1543454645aa299
denkin14 <dennismuguongo@gmail.com>
2022-07-14 17:06:45 +0000

    stacks(lifo and fifo)queues

--- monty.h
+++ monty.h
@@ -1,12 +1,17 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
-#include <ctype.h>
+extern int num;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -14,7 +19,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -22,57 +27,35 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: the  pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: will carry values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
+
 /**
  * struct instruction_s - opcode and its function
- * @opcode: opcode
- * @f: the function to handle the opcode
+ * @opcode: the opcode
+ * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
-#endif
+
+void pushstack(stack_t **structstack, unsigned int lineno);
+void pallstack(stack_t **structstack, unsigned int lineno);
+void peekstack(stack_t **structstack, unsigned int lineno);
+void comS(stack_t **structstack, unsigned int lineno);
+
+
+void dellineno(char **buf);
+void tokenizer(char **buf, char ***tokens);
+void instructchoose(stack_t **structstack, char ***tokens, unsigned int lineno);
+void checknumber(char *n, unsigned int lineno);
+void (*choose_operand(char *code))(stack_t **structstack, unsigned int lineno);
+void monty_inter(char **av);
+void freetokens(char ***tokens);
+void free_structstack(stack_t *structstack);
+void line_token(char **buf, char ***tokens, ssize_t readline);
+
+#endif /* MONTY_H */
